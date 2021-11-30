d0efcfb86e80b354dd8d03cb4a793951262418ba
samir <s-malik@live.co.uk>
2021-11-30 15:34:16 +0000

    3rd Test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(number)
-    if number == 3 then 'fizz'
+    if number % 3 == 0 && number % 5 == 0 then 'fizzbuzz'
+    elsif number == 3 then 'fizz'
     elsif number == 5 then 'buzz'
+    else number 
     end 
 end
